2df1380280f0b9e1f451834555105e41b084fa18
princegodwyll15 <godwyllprince@gmail.com>
2025-07-08 22:45:37 +0000

    next app startup

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return (
+        <h1>Hi this is Customer Page</h1>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return (
+        <h1>Hi this is Invoice Page</h1>
+    )
+}
